0c88fd921bf2f02ec69993cbe415c7da61406de7
Andrey Belov <Andrey.Belov@smartds.ru>
2024-08-13 12:22:14 +0500

    hoho

--- com/java/test/Main.java
+++ com/java/test/Main.java
@@ -3,5 +3,8 @@ package com.java.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hehe");
+        System.out.println("hoho");
+        System.out.println("hehe");
+        System.out.println("hoho");
     }
 }
